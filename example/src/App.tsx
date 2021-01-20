a64eef8967408ff9cd64b1c17b35278348e014d9
Stijn Rogiest <reddusted@gmail.com>
2021-01-20 16:01:41 +0100

    Use VisualRender for example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -7,24 +7,23 @@ import {
     Listener,
     useForm
 } from './StateForm'
+import { VisualRender } from './VisualRender'
 
 function FormVisualize<T>(props: { form: FormState<T> }) {
     return (
         <AnyListener
             form={props.form}
             render={({ values }) => (
-                <div style={{ background: '#eee' }}>
-                    <pre>{JSON.stringify(values, null, 2)}</pre>
-                    {props.form.isDirty && (
-                        <p>
-                            <strong>DIRTY</strong>
-                        </p>
-                    )}
-                    <pre>
-                        {JSON.stringify(props.form.values)}{' '}
-                        {JSON.stringify(props.form.defaultValues)}
-                    </pre>
-                </div>
+                <VisualRender>
+                    <div style={{ background: '#eee' }}>
+                        <pre>{JSON.stringify(values, null, 2)}</pre>
+                        {props.form.isDirty && (
+                            <p>
+                                <strong>DIRTY</strong>
+                            </p>
+                        )}
+                    </div>
+                </VisualRender>
             )}
         />
     )
@@ -45,96 +44,104 @@ export default function App() {
     const form = useForm(defaultValues)
 
     return (
-        <form
-            style={{ margin: '3em' }}
-            onSubmit={(ev) => {
-                ev.preventDefault()
-                setDefaultValues(form.values)
-                console.log('submit', form.values)
-            }}
-        >
-            <Listener
-                form={form}
-                name='firstName'
-                render={({ value, setValue }) => (
-                    <input
-                        value={value}
-                        onChange={(ev) => setValue(ev.target.value)}
-                    />
-                )}
-            />
-            <ArrayField
-                name='todo'
-                parent={form}
-                render={({ values, append, remove, form }) => (
-                    <>
-                        <ul>
-                            {values.map((_, i) => (
-                                <ChildForm
-                                    key={i}
-                                    parent={form}
-                                    name={i}
-                                    render={(form) => (
-                                        <li>
-                                            <FormVisualize form={form} />
+        <VisualRender>
+            <form
+                style={{ margin: '3em' }}
+                onSubmit={(ev) => {
+                    ev.preventDefault()
+                    setDefaultValues(form.values)
+                    console.log('submit', form.values)
+                }}
+            >
+                <Listener
+                    form={form}
+                    name='firstName'
+                    render={({ value, setValue }) => (
+                        <input
+                            value={value}
+                            onChange={(ev) => setValue(ev.target.value)}
+                        />
+                    )}
+                />
+                <ArrayField
+                    name='todo'
+                    parent={form}
+                    render={({ values, append, remove, form }) => (
+                        <VisualRender>
+                            <ul>
+                                {values.map((_, i) => (
+                                    <ChildForm
+                                        key={i}
+                                        parent={form}
+                                        name={i}
+                                        render={(form) => (
+                                            <li>
+                                                <Listener
+                                                    form={form}
+                                                    name='title'
+                                                    render={({
+                                                        value,
+                                                        setValue,
+                                                        dirty
+                                                    }) => (
+                                                        <VisualRender>
+                                                            <input
+                                                                style={{
+                                                                    background: dirty
+                                                                        ? '#f99'
+                                                                        : '#fff'
+                                                                }}
+                                                                value={value}
+                                                                onChange={(
+                                                                    ev
+                                                                ) =>
+                                                                    setValue(
+                                                                        ev
+                                                                            .target
+                                                                            .value
+                                                                    )
+                                                                }
+                                                            />
+                                                        </VisualRender>
+                                                    )}
+                                                />
+                                                <button
+                                                    type='button'
+                                                    onClick={() => remove(i)}
+                                                >
+                                                    remove
+                                                </button>
+                                            </li>
+                                        )}
+                                    />
+                                ))}
+                            </ul>
+                            <button
+                                type='button'
+                                onClick={() => {
+                                    let input = window.prompt(
+                                        'Enter new todo item'
+                                    )
+                                    if (input)
+                                        append({
+                                            title: input,
+                                            id: '' + new Date().getTime()
+                                        })
+                                }}
+                            >
+                                Add todo item
+                            </button>
+                        </VisualRender>
+                    )}
+                />
 
-                                            <Listener
-                                                form={form}
-                                                name='title'
-                                                render={({
-                                                    value,
-                                                    setValue,
-                                                    dirty
-                                                }) => (
-                                                    <input
-                                                        style={{
-                                                            background: dirty
-                                                                ? '#f99'
-                                                                : '#fff'
-                                                        }}
-                                                        value={value}
-                                                        onChange={(ev) =>
-                                                            setValue(
-                                                                ev.target.value
-                                                            )
-                                                        }
-                                                    />
-                                                )}
-                                            />
-                                            <button
-                                                type='button'
-                                                onClick={() => remove(i)}
-                                            >
-                                                remove
-                                            </button>
-                                        </li>
-                                    )}
-                                />
-                            ))}
-                        </ul>
-                        <button
-                            type='button'
-                            onClick={() => {
-                                let input = window.prompt('Enter new todo item')
-                                if (input)
-                                    append({
-                                        title: input,
-                                        id: '' + new Date().getTime()
-                                    })
-                            }}
-                        >
-                            Add todo item
-                        </button>
-                    </>
-                )}
-            />
+                <FormVisualize form={form} />
 
-            <FormVisualize form={form} />
-
-            <button>submit</button>
-            <button type='button' onClick={() => form.reset()}>
-                reset
-            </button>
-        </form>
+                <button>submit</button>
+                <button type='button' onClick={() => form.reset()}>
+                    reset
+                </button>
+            </form>
+        </VisualRender>
     )
 }
---
+++ example/src/VisualRender.tsx
@@ -0,0 +1,17 @@
+import React, { useRef } from 'react'
+
+export function VisualRender(props: { children: React.ReactNode }) {
+    let ref = useRef<HTMLDivElement>(null)
+    let timeoutRef = useRef<number | null>(null)
+
+    if (ref.current) {
+        ref.current.className = ''
+        if (timeoutRef.current) clearTimeout(timeoutRef.current)
+        timeoutRef.current = window.setTimeout(
+            () => (ref.current!.className = 'blink'),
+            100
+        )
+    }
+
+    return <div ref={ref}>{props.children}</div>
+}
