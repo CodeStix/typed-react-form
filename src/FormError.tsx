9134ba4bef606a28b991ea38bc3fe44860f34212
Stijn Rogiest <reddusted@gmail.com>
2021-02-12 19:02:16 +0100

    Remove FormError.deserializer

--- src/FormError.tsx
+++ src/FormError.tsx
@@ -1,15 +1,14 @@
 import React, { HTMLAttributes } from "react";
-import { ErrorType, FormState } from "./form";
+import { FormState } from "./form";
 import { useListener } from "./hooks";
 
 export type FormErrorProps<T, Error, Key extends keyof T> = Omit<HTMLAttributes<HTMLParagraphElement>, "name" | "form"> & {
     form: FormState<T, any, Error>;
     name: Key;
-    deserializer?: (error: ErrorType<T[Key], Error> | undefined) => React.ReactNode;
 };
 
-export function FormError<T, Error, Key extends keyof T>({ form, name, deserializer, ...rest }: FormErrorProps<T, Error, Key>) {
+export function FormError<T, Error, Key extends keyof T>({ form, name, ...rest }: FormErrorProps<T, Error, Key>) {
     const { error } = useListener(form, name);
     if (!form.error) return null;
-    return <p {...rest}>{deserializer ? deserializer(error) : error}</p>;
+    return <p {...rest}>{error + ""}</p>;
 }
