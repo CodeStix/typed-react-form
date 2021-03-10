d363b5671c6198dbae1a4e381ee7bac94959bad6
Stijn Rogiest <reddusted@gmail.com>
2021-03-10 16:21:42 +0100

    Add router to example

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -2,7 +2,16 @@ import "./index.css";
 
 import React from "react";
 import ReactDOM from "react-dom";
-// import App from "./App";
-import App from "./OneOfObjectForm";
+import App from "./App";
+import OneOfObjectForm from "./OneOfObjectForm";
+import OneOfObjectArrayForm from "./OneOfObjectArrayForm";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <Router>
+        <Route path="/" exact component={App} />
+        <Route path="/object-types" exact component={OneOfObjectForm} />
+        <Route path="/object-types-array" exact component={OneOfObjectArrayForm} />
+    </Router>,
+    document.getElementById("root")
+);
